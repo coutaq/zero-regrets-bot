b75ba9816ae564f4c2d4e6ea7ccd172f76a5aa70
Michael Melikov <coutaq@gmail.com>
2020-03-16 18:13:25 +0300

    Add project files.

---
+++ ZeroRegretsBot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace Awesome
+{
+    class Program
+    {
+        static ITelegramBotClient botClient;
+
+        static void Main()
+        {
+            botClient = new TelegramBotClient("1127968745:AAGURMsF2N-4a-fuTbSUrtWbaOxxfaGol6I");
+            var me = botClient.GetMeAsync().Result;
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+        }
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            Console.WriteLine(e.Message.Text);
+            if (e.Message.Text == "шиз")
+            {
+                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+                await botClient.SendTextMessageAsync(e.Message.Chat, "тех");
+            }
+        }
+    }
+}
